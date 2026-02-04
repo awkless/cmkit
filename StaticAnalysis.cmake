3688ddf560ef95b6a423f595da47191446572108
Jason Pena <jasonpena@awkless.com>
2026-02-04 16:57:58 -0700

    fix: pass ARGN as a list not as a string
    Did not properly pass ARGN to the custom target `run_clang_tidy`.
    I also tied the target to ALL to ensure that it always runs.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- StaticAnalysis.cmake
+++ StaticAnalysis.cmake
@@ -20,9 +20,11 @@ function(lint_with_clang_tidy)
     set(CMAKE_EXPORT_COMPILE_COMMANDS ON PARENT_SCOPE)
     find_program(CLANG_TIDY_EXE NAMES clang-tidy REQUIRED)
     set(COMPILE_COMMANDS_PATH "${CMAKE_BINARY_DIR}/compile_commands.json")
+
     add_custom_target(
         run_clang_tidy
-        COMMAND ${CLANG_TIDY_EXE} "-p" "${CMAKE_BINARY_DIR}" "${ARGN}"
+        ALL
+        COMMAND ${CLANG_TIDY_EXE} "-p" "${CMAKE_BINARY_DIR}" ${ARGN}
 	DEPENDS "${COMPILE_COMMANDS_PATH}"
         WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
         COMMENT "Run clang-tidy analysis"
